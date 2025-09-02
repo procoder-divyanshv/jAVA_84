f211e0b3a7407e8e0f6781eb90182a9475d4f8fb
DivyanshVerma <divyanshverma1616@gmail.com>
2025-09-02 12:22:24 +0530

    2 September, using iterator for collection in java

--- src/Unit3/ArrayList2.java
+++ src/Unit3/ArrayList2.java
@@ -7,17 +7,17 @@ import java.util.Iterator;
 
 public class ArrayList2 {
     public static void main(String[] args) {
-        ArrayList<String> a2= new ArrayList<>();
-        a2.add("A");
-        a2.add("b");
-        a2.add("C");
-        a2.add("d");
+        ArrayList<Integer> a2= new ArrayList<>();
+        a2.add(1);
+        a2.add(4);
+        a2.add(3);
+        a2.add(2);
         System.out.println(a2);
 //        for(String val: a2){
 //            System.out.print(val+" ");
 //        }
         Collections.sort(a2);
-        Iterator<String>it= a2.iterator();
+        Iterator<Integer>it= a2.iterator();
 
         while(it.hasNext()){ //this is the print of the collection by using the iterator in java
 
